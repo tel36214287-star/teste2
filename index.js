8e95158aed4602272c811622f57939442ca66b96
D-Renato <tel36214287@gmail.com>
2025-12-12 10:45:19 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+[
+    {
+        "id_quarto": 101,
+        "tipo": "Standard",
+        "capacidade": 2,
+        "preco_diaria": 150.0,
+        "status": "OCUPADO",
+        "hospede_nome": "Dona Jussara Silva",
+        "check_out_previsto": "2025-12-14"
+    },
+    {
+        "id_quarto": 102,
+        "tipo": "Standard",
+        "capacidade": 2,
+        "preco_diaria": 150.0,
+        "status": "VAGO",
+        "hospede_nome": null,
+        "check_out_previsto": null
+    },
+    {
+        "id_quarto": 201,
+        "tipo": "Luxo",
+        "capacidade": 3,
+        "preco_diaria": 250.0,
+        "status": "OCUPADO",
+        "hospede_nome": "Sr. João Cliente",
+        "check_out_previsto": "2025-12-12"
+    },
+    {
+        "id_quarto": 202,
+        "tipo": "Luxo",
+        "capacidade": 3,
+        "preco_diaria": 250.0,
+        "status": "VAGO",
+        "hospede_nome": null,
+        "check_out_previsto": null
+    },
+    {
+        "id_quarto": 301,
+        "tipo": "Suíte Master",
+        "capacidade": 4,
+        "preco_diaria": 400.0,
+        "status": "VAGO",
+        "hospede_nome": null,
+        "check_out_previsto": null
+    }
+]
